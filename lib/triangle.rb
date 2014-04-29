93080fa61211c55654cc886fdd1f850a4d8d1a3c
Alex Grant <grantovich@users.noreply.github.com>
2014-04-29 13:00:48 -0400

    Triangles can calculate their area

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,7 +6,12 @@ class Triangle
   end
 
   def perimeter
-    @sides[0] + @sides[1] + @sides[2]
+    sides[0] + sides[1] + sides[2]
     # Advanced version: @sides.reduce(&:+)
+  end
+
+  def area
+    hp = perimeter * 0.5 # Half the perimeter
+    Math.sqrt(hp * (hp - sides[0]) * (hp - sides[1]) * (hp - sides[2]))
   end
 end
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -17,4 +17,12 @@ describe Triangle do
     expect(first_triangle.perimeter).to eq 15
     expect(second_triangle.perimeter).to eq 35
   end
+
+  it 'calculates its area' do
+    first_triangle = Triangle.new(3, 4, 5)
+    second_triangle = Triangle.new(7, 5, 10)
+
+    expect(first_triangle.area).to eq 6
+    expect(second_triangle.area).to be_within(0.01).of(16.25)
+  end
 end
