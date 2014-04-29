23b025b3487eb5354c4e56db353869026e457e6d
Alex Grant <grantovich@users.noreply.github.com>
2014-04-29 14:30:04 -0400

    Bonus validation for rectangles

--- lib/rectangle.rb
+++ lib/rectangle.rb
@@ -2,6 +2,9 @@ class Rectangle
   attr_reader :length, :width
 
   def initialize(length:, width:)
+    if length <= 0 || width <= 0
+      raise ArgumentError, 'length and width must be greater than 0'
+    end
     @length, @width = length, width
   end
 
--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -14,6 +14,11 @@ describe Rectangle do
     expect(@second_rectangle.width).to eq 23
   end
 
+  it 'rejects invalid values for length and width' do
+    expect{ Rectangle.new(length: 10, width: 0) }.to raise_error ArgumentError
+    expect{ Rectangle.new(length: -2, width: 5) }.to raise_error ArgumentError
+  end
+
   it 'calculates its perimeter' do
     expect(@first_rectangle.perimeter).to eq 30
     expect(@second_rectangle.perimeter).to eq 74
