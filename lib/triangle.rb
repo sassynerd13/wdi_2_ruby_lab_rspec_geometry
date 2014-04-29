7009383d29c7645a568ba6678b6d3a554613df94
Alex Grant <grantovich@users.noreply.github.com>
2014-04-29 12:54:13 -0400

    Triangles can calculate their perimeter

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,4 +4,9 @@ class Triangle
   def initialize(side_a, side_b, side_c)
     @sides = [side_a, side_b, side_c]
   end
+
+  def perimeter
+    @sides[0] + @sides[1] + @sides[2]
+    # Advanced version: @sides.reduce(&:+)
+  end
 end
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -9,4 +9,12 @@ describe Triangle do
     expect(first_triangle.sides).to eq [2, 6, 7]
     expect(second_triangle.sides).to eq [8, 16, 11]
   end
+
+  it 'calculates its perimeter' do
+    first_triangle = Triangle.new(2, 6, 7)
+    second_triangle = Triangle.new(8, 16, 11)
+
+    expect(first_triangle.perimeter).to eq 15
+    expect(second_triangle.perimeter).to eq 35
+  end
 end
